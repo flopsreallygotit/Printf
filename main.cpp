5ed0bdc9b1385a55e9c3cbf2e94c80f7e8924bbb
flopsreallygotit <flopsreallygotit@gmail.com>
2023-03-19 15:46:17 +0300

    %d %o %x %b added.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@ extern "C" void _printf(const char *str, ...);
 
 int main()
 {
-    _printf("%%Hello world%c\n%s\n", '!', "Test string");
+    // _printf("PERCENT: \t%%\nSYMBOL: \t%c\nSTRING: \t%s\nSTRING: \t%s\n", '!', "ABOBA", "BEBRA");
+
+    _printf("%a");
+
+    // _printf("PERCENT: \t%%\nSYMBOL: \t%c\nSTRING: \t%s\nNUMBER: \t%d\n", '!', "ABOBA", 1234);
 
     return 0;
 }
--- printf.s
+++ printf.s
@@ -25,10 +25,11 @@ times ('x' - 's' - 1)   dq __error
                         dq __hex
 
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-;~~~Buffers~~~
+;~~~Buffer~~~
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Buffer: times 32 db 48
+Buffer:         times 64 db 48
+BufferLength:   equ $ - Buffer
 
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;~~~Error Message~~~
@@ -36,14 +37,6 @@ Buffer: times 32 db 48
 
 Error:          db '~~~ERROR~~~'
 ErrorLength:    equ $ - Error
-
-;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-;~~~Test Data~~~
-;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-Specificator:   db 'aboba%% %b', 10, 0
-
-String:         db '12345', 0
 
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -68,23 +61,23 @@ _printf:        push r9         ;\
                 push r8         ;|
                 push rcx        ;+------> First six arguments are in these regs 
                 push rdx        ;|        and other are in stack. So we push them
-                push rsi	    ;+------> for comfortable use in future.
+                push rsi	    ;+------> for comfortable use in future
                 push rdi        ;/
 
-                push rbp        ; Saving base pointer.
-                mov rbp, rsp    ; Putting stack pointer to base pointer.
-                add rbp, 16	    ; Skipping rbp and string with specificators in string.
+                push rbp        ; Saving base pointer
+                mov rbp, rsp    ; Putting stack pointer to base pointer
+                add rbp, 16	    ; Skipping rbp and string with specificators in string
 
-                mov rsi, rdi    ; Now RSI = Address of template string.
+                mov rsi, rdi    ; Now RSI = Address of template string
 
-                call __printf   ; Calling main function.
+                call __printf   ; Calling main function
 
-                pop rbp         ; Restoring base pointer.
+                pop rbp         ; Restoring base pointer
 
                 pop  rdi        ;\
                 pop  rsi        ;|
                 pop  rdx        ;|
-                pop  rcx        ;+------> Restoring regs with arguments.
+                pop  rcx        ;+------> Restoring regs with arguments
                 pop  r8         ;|
                 pop  r9         ;/
 
@@ -102,17 +95,17 @@ _printf:        push r9         ;\
 
 __printf:       push rax            ; Saving RAX
 
-.next:          mov al, [rsi]       ; Putting current template string symbol to AL.
+.next:          mov al, [rsi]       ; Putting current template string symbol to AL
 
-                cmp al, 0           ; If AL == 0 then it's end of template string.
+                cmp al, 0           ; If AL == 0 then it's end of template string
                 je .end
 
-                cmp al, 37  ; '%'   ; If current symbol is % then it's specificator.
+                cmp al, 37  ; '%'   ; If current symbol is % then it's specificator
                 je _hndl_spfr
 
-                PUTCHAR             ; If it's ordinary symbol then output it.
+                PUTCHAR             ; If it's ordinary symbol then output it
 
-                inc rsi             ; Moving to address of next symbol.
+                inc rsi             ; Moving to address of next symbol
                 jmp .next
 
 .end:           pop  rax
@@ -128,13 +121,13 @@ __printf:       push rax            ; Saving RAX
 ;           Function __error
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-_hndl_spfr:     inc rsi             ; Moving to letter address.
-                mov al, [rsi]       ; Putting current symbol to AL.
+_hndl_spfr:     inc rsi             ; Moving to letter address
+                mov al, [rsi]       ; Putting current symbol to AL
 
                 cmp al, '%'
                 jne .handle
 
-                PUTCHAR             ; '%%' -> '%' at output.
+                PUTCHAR             ; '%%' -> '%' at output
                 jmp .end
 
 .handle:        cmp al, 0
@@ -142,11 +135,11 @@ _hndl_spfr:     inc rsi             ; Moving to letter address.
 
                 cmp al, 'b'         ;\
                 jb __error          ;|
-                                    ;+------> Checking that letter is in range 'b' - 'x'.
+                                    ;+------> Checking that letter is in range 'b' - 'x'
                 cmp al, 'x'         ;|
                 ja __error          ;/
 
-                mov rax, [jump_table + (rax - 'b') * 8] ; Jumping to function that handles that letter.
+                mov rax, [jump_table + (rax - 'b') * 8] ; Jumping to function that handles that letter
                 jmp rax
 
 .end:           inc rsi
@@ -157,7 +150,21 @@ _hndl_spfr:     inc rsi             ; Moving to letter address.
 ;~~~Specificators~~~
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-__binary:       ; TODO
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+; Writes binary form of number in output
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+; Entry:    Number in stack
+; Exit:     None
+; Destroys: RAX, RBX
+; Expects:  None
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+__binary:       mov rax, [rbp]  ; Popping out current argument
+                add rbp, 8      ; Moving base pointer to next argument
+
+                mov rbx, 2
+                call Itoa
+
                 jmp _hndl_spfr.end
 
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -169,10 +176,10 @@ __binary:       ; TODO
 ; Expects:  Memory buffer 'Buffer' (1 byte at least)
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-__char:         push rsi
+__char:         mov rax, [rbp]  ; Popping out current argument
+                add rbp, 8      ; Moving base pointer to next argument
 
-                mov rax, [rbp]  ; Popping out current argument.
-                add rbp, 8      ; Moving base pointer to next argument.
+                push rsi
 
                 mov rsi, Buffer	
                 mov byte [rsi], al
@@ -183,10 +190,38 @@ __char:         push rsi
 
                 jmp _hndl_spfr.end
 
-__decimal:      
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+; Writes decimal to output
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+; Entry:    Decimal in stack
+; Exit:     None
+; Destroys: RAX, RBX
+; Expects:  None
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+__decimal:      mov rax, [rbp]  ; Popping out current argument
+                add rbp, 8      ; Moving base pointer to next argument
+
+                mov rbx, 10
+                call Itoa
+
                 jmp _hndl_spfr.end
 
-__octal:        
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+; Writes decimal to output
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+; Entry:    Decimal in stack
+; Exit:     None
+; Destroys: RAX, RBX
+; Expects:  Memory buffer 'Buffer' (1 byte at least)
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+__octal:        mov rax, [rbp]  ; Popping out current argument
+                add rbp, 8      ; Moving base pointer to next argument
+
+                mov rbx, 8
+                call Itoa
+
                 jmp _hndl_spfr.end
 
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -216,11 +251,16 @@ __string:       push rsi
 
                 jmp _hndl_spfr.end
 
-__hex:          
+__hex:          mov rax, [rbp]  ; Popping out current argument
+                add rbp, 8      ; Moving base pointer to next argument
+
+                mov rbx, 16
+                call Itoa
+
                 jmp _hndl_spfr.end
 
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-; Writes error to output.
+; Writes error to output
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ; Entry:    None
 ; Exit:     None
@@ -232,6 +272,56 @@ __hex:
 
 __error:        WRITE Error, ErrorLength
                 jmp _hndl_spfr.end
+
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+; Translates number with given radix to string format
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+; Entry:    EAX = Number, EBX = Radix, RSI = Buffer
+; Exit:     Reversed number in string format in buffer
+; Destroys: None
+; Expects:  Memory buffer 'Buffer' (20 bytes recommended or bigger)
+;           Constant length of buffer 'BufferLength'
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Itoa:       push rsi
+            push rcx
+            push rdx
+
+            mov rsi, Buffer + BufferLength - 1  ; Last symbol of buffer
+            xor rcx, rcx
+
+.divide:    xor edx, edx
+            div ebx
+            add edx, '0'
+
+            cmp edx, '9'
+            ja .letter
+
+            jmp .continue
+
+.letter:    add edx, 'A' - '0' - 10
+
+.continue:  mov [rsi], dl
+
+            dec rsi
+            inc rcx
+
+            cmp eax, 0
+            jnz .divide
+
+            inc rsi
+
+.next:      PUTCHAR
+
+            inc rsi
+            dec rcx
+            jnz .next
+
+            pop rdx
+            pop rcx
+            pop rsi
+
+            ret     
 
 ; ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ; ; Entry:    RSI = Address of 0-terminated string
